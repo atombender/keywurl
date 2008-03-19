98431ebd7bba80cd15c4d2e4cbe07b31db9fd191
Alexander Staubo <alex@purefiction.net>
2008-03-19 21:29:51 +0100

    Version number, continued.

---
+++ Constants.h
@@ -0,0 +1,18 @@
+// Keywurl version
+#define KEYWURL_MAJORVERSION 1
+#define KEYWURL_MINORVERSION 4
+#define KEYWURL_MAINTVERSION 0
+
+// Symbols needed for Info.plist; ugly workaround to avoid whitespace
+#define MACRO_PASTE_5(a, b, c, d, e) a##b##c##d##e
+#define MACRO_PASTE_5A(a, b, c, d, e) MACRO_PASTE_5(a, b, c, d, e)
+#define KEYWURL_VERSION_STRING MACRO_PASTE_5A(KEYWURL_MAJORVERSION, ., KEYWURL_MINORVERSION, ., KEYWURL_MAINTVERSION)
+
+// Bundle min and max versions
+#ifdef KEYWURL_TARGET_TIGER
+    #define KEYWURL_MIN_SAFARI_VERSION 523
+    #define KEYWURL_MAX_SAFARI_VERSION 4525
+#else
+    #define KEYWURL_MIN_SAFARI_VERSION 5523
+    #define KEYWURL_MAX_SAFARI_VERSION 5525
+#endif
