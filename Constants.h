d3ee2043b34d07814fd5bf974fe30467252080b6
Alexander Staubo <alex@bengler.no>
2009-06-12 19:40:07 +0200

    Updated bundle min/max versions to Safari 4, cleaned up project file constants.

--- Constants.h
+++ Constants.h
@@ -11,15 +11,22 @@
 
 // Bundle min and max versions
 #ifdef KEYWURL_TARGET_TIGER
-    #define KEYWURL_MIN_SAFARI_VERSION 523
-    #define KEYWURL_MAX_SAFARI_VERSION 4525
+    #if KEYWURL_SAFARI_VERSION == 3
+        #define KEYWURL_MIN_SAFARI_VERSION 523
+        #define KEYWURL_MAX_SAFARI_VERSION 4525
+    #elif KEYWURL_SAFARI_VERSION == 4
+        #define KEYWURL_MIN_SAFARI_VERSION 4530
+        #define KEYWURL_MAX_SAFARI_VERSION 4530
+    #else
+        #error "You need to define KEYWURL_SAFARI_VERSION correctly"
+    #endif
 #else
     #if KEYWURL_SAFARI_VERSION == 3
         #define KEYWURL_MIN_SAFARI_VERSION 5523
         #define KEYWURL_MAX_SAFARI_VERSION 5525
     #elif KEYWURL_SAFARI_VERSION == 4
-        #define KEYWURL_MIN_SAFARI_VERSION 5528
-        #define KEYWURL_MAX_SAFARI_VERSION 5528
+        #define KEYWURL_MIN_SAFARI_VERSION 5530
+        #define KEYWURL_MAX_SAFARI_VERSION 5530
     #else
         #error "You need to define KEYWURL_SAFARI_VERSION correctly"
     #endif
