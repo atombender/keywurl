e2450066515aaa1d82adaa5b03af1626d717fa38
nox <nox@dev-extend.com>
2009-09-12 03:54:57 +0200

    Do not use class posing in KeywurlBrowserWebView.

--- KeywurlBrowserWebView.h
+++ KeywurlBrowserWebView.h
@@ -1,5 +1,11 @@
 #import <Cocoa/Cocoa.h>
 #import "Safari.h"
 
+#ifdef __OBJC2__
+@interface BrowserWebView (Keywurl_BrowserWebView)
++ (void) _Keywurl_load;
+@end
+#else
 @interface KeywurlBrowserWebView : BrowserWebView
 @end
+#endif
--- KeywurlBrowserWebView.m
+++ KeywurlBrowserWebView.m
@@ -1,20 +1,53 @@
+#ifdef __OBJC2__
+#include <objc/objc.h>
+#include <objc/runtime.h>
+#endif
+
 #include "KeywurlBrowserWebView.h"
 #include "KeywurlBrowserWindowController.h"
 #include "KeywurlPlugin.h"
 
+#ifdef __OBJC2__
+@implementation BrowserWebView (BrowserWebView_KeywurlPlugin)
+#else
 @implementation KeywurlBrowserWebView
+#endif
+
+#ifdef __OBJC2__
++ (void) _Keywurl_load
+{
+	Method old, new;
+	Class c = [self class];
+	
+	old = class_getInstanceMethod(c, @selector(fallbackURLs));
+	new = class_getInstanceMethod(c, @selector(_Keywurl_fallbackURLs));
+	method_exchangeImplementations(old, new);
+	
+	old = class_getInstanceMethod(c, @selector(webView));
+	new = class_getInstanceMethod(c, @selector(_Keywurl_webView));
+	method_exchangeImplementations(old, new);
+}
+#endif
 
 #if KEYWURL_SAFARI_VERSION >= 4
 
+#ifdef __OBJC2__
+- (NSArray*) _Keywurl_fallbackURLs {
+#else
 - (NSArray*) fallbackURLs {
+#endif
     // If Safari has failed to resolve the address into a proper host name, the
     // list of fallback URLs is empty and we can intercept the URL and map it
+#ifdef __OBJC2__
+    NSArray* urls = [self _Keywurl_fallbackURLs];
+#else
     NSArray* urls = [super fallbackURLs];
+#endif
     if (urls == nil || [urls count] == 0) {
     	KeywurlPlugin* plugin = [KeywurlPlugin sharedInstance];
         KeywordMapper* mapper = [plugin keywordMapper];
         LocationFieldEditor* fieldEditor = (LocationFieldEditor*) [
-          (KeywurlBrowserWindowController*) [self windowController] keywurl_locationFieldEditor];
+          [self windowController] keywurl_locationFieldEditor];
     	NSString* address = [[fieldEditor textStorage] string];
         NSString* mapped = [mapper mapKeywordInput: address];
     	if (mapped && ![mapped isEqualToString: address]) {
@@ -38,13 +71,21 @@
         NSURL* mappedUrl = [NSURL URLWithString: mapped];
         return [NSArray arrayWithObject: mappedUrl];
     } else {
+#ifdef __OBJC2__
+        return [self _Keywurl_fallbackURLs];
+#else
         return [super fallbackURLs];
+#endif
     }
 }
 
 #endif
 
+#ifdef __OBJC2__
+- (id) _Keywurl_webView: (id) sender 
+#else
 - (id) webView: (id) sender 
+#endif
     contextMenuItemsForElement: (id) elementDictionary
     defaultMenuItems: (NSMutableArray*) defaultMenuItems {
     NSString* documentUrl = [self mainFrameURL];
@@ -59,7 +100,11 @@
         [item setTarget: plugin];
         [defaultMenuItems addObject: item];
     }
+#ifdef __OBJC2__
+	return [self _Keywurl_webView: sender
+#else
     return [super webView: sender
+#endif
          contextMenuItemsForElement: elementDictionary
          defaultMenuItems: defaultMenuItems];
 }
